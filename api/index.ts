607c1b55cb04d03c4581ce788ec86e7f35dae9d9
akwancakra <akwancakra20@gmail.com>
2024-12-03 20:53:11 +0700

    fix: Update Hardhat configuration to use absolute paths and ensure temporary directory exists

--- api/index.ts
+++ api/index.ts
@@ -1,1 +1,7 @@
 import '../src/main';
+import fs from 'fs';
+
+const tmpDir = '/tmp/hardhat';
+if (!fs.existsSync(tmpDir)) {
+  fs.mkdirSync(tmpDir, { recursive: true });
+}
--- hardhat.config.ts
+++ hardhat.config.ts
@@ -15,16 +15,52 @@ const config: HardhatUserConfig = {
     },
   },
   paths: {
-    artifacts: 'tmp/hardhat/artifacts',
-    cache: 'tmp/hardhat/cache',
-    sources: 'tmp/hardhat/contracts',
-    ignition: 'tmp/hardhat/ignition',
+    artifacts: '/tmp/hardhat/artifacts',
+    cache: '/tmp/hardhat/cache',
+    sources: '/tmp/hardhat/contracts',
+    ignition: '/tmp/hardhat/ignition',
   },
   defaultNetwork: 'hardhat',
   typechain: {
-    outDir: 'tmp/hardhat/typechain-types',
+    outDir: '/tmp/hardhat/typechain-types',
     target: 'ethers-v6',
   },
 };
 
 export default config;
+
+// paths: {
+//   artifacts: './src/artifacts',
+//   cache: './src/cache',
+//   sources: './src/contracts',
+//   ignition: './src/ignition',
+// },
+// defaultNetwork: 'hardhat',
+// typechain: {
+//   outDir: 'typechain-types',
+//   target: 'ethers-v6',
+// },
+
+// solidity: '0.8.27',
+//   networks: {
+//     hardhat: {
+//       chainId: 1337,
+//       // accounts: [
+//       //   {
+//       //     privateKey: process.env.PRIVATE_KEY || '',
+//       //     balance: '1000000000000000000000',
+//       //   },
+//       // ],
+//     },
+//   },
+//   paths: {
+//     artifacts: 'tmp/hardhat/artifacts',
+//     cache: 'tmp/hardhat/cache',
+//     sources: 'tmp/hardhat/contracts',
+//     ignition: 'tmp/hardhat/ignition',
+//   },
+//   defaultNetwork: 'localhost',
+//   typechain: {
+//     outDir: 'tmp/hardhat/typechain-types',
+//     target: 'ethers-v6',
+//   },
